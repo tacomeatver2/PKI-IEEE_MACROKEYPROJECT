dc9619129178b31998abbe47d2674e6aefd06cab
Takumi Ito <nekotakumi@gmail.com>
2019-10-12 15:05:32 -0700

    Update MACROKEY_PROJECT.ino

--- MACROKEY_PROJECT/MACROKEY_PROJECT.ino
+++ MACROKEY_PROJECT/MACROKEY_PROJECT.ino
@@ -1,7 +1,7 @@
-//MACROKEY PROJECT
-//PKI-IEEE
+
+//MACRO_KEYPAD
 //Takumi Ito
-//9/30/19
+//10/12/19
 //This is a code to control an aruduino micro to send commands to the PC based on the key pressed on the keypad.
 
 //////////////////////////////////////////////////////
@@ -10,7 +10,9 @@
 #include <Encoder.h>
 #include <HID-Project.h>
 //import library https://github.com/NicoHood/HID
-//
+//import library https://github.com/PaulStoffregen/Encoder
+// or click Tools>> Manage libraries >> search for HID >> scroll down until you see HID-Project by NicoHood version 2.6.1 >> then install that if you are unsure of how to import the libraries correctly
+// same steps but search for encoder >> scroll down until you see Encoder by PaulStoffregen verson 1.4.1 >> then install it.
 
 #define B1 1
 #define B2 0
@@ -25,7 +27,7 @@
 Encoder myEnc(15,14);
 const int pinLed = LED_BUILTIN;
 const int pinButton = 16;
-int bulbPin = 9; 
+int bulbPin = 9;
 int oldPosition  = -999;
 boolean muted = 0;
 int safePosition = 0;
@@ -34,6 +36,7 @@ int oldVolume = 0;
 int actualVolume = 0;
 String app;
 String url;
+boolean modeos = 0; //mac = 0 win = 1
 
 //////////////////////////////////////////////////////
 //Setup
@@ -48,17 +51,17 @@ Serial.print("set ");
   pinMode (B5, INPUT_PULLUP);
   pinMode (B6, INPUT_PULLUP);
 
-  
+
   // initialize the LED pin as an output:
   pinMode(L1, OUTPUT);
   pinMode(L2, OUTPUT);
   digitalWrite(L2, HIGH);
-  
+
 
   Serial.begin(9600);
   Consumer.begin();
   delay(1000);
-  
+
   for(int i = 50; i > 0; i--){
   Consumer.write(MEDIA_VOLUME_DOWN);
   delay(1);
@@ -69,100 +72,209 @@ Serial.print("set ");
 //Loop
 //////////////////////////////////////////////////////
 void loop() {
-  int newPosition = myEnc.read();
+  if (modeos = 0){
+    int newPosition = myEnc.read();
 
-  if (newPosition != oldPosition) {
-    Serial.println("new");
-    safePosition = newPosition;
-    if(newPosition < 0){
-      safePosition = 0;
-      myEnc.write(safePosition);
+    if (newPosition != oldPosition) {
+      Serial.println("new");
+      safePosition = newPosition;
+      if(newPosition < 0){
+        safePosition = 0;
+        myEnc.write(safePosition);
+      }
+      if(newPosition > 400){
+        safePosition = 400;
+        myEnc.write(safePosition);
+      }
+      oldPosition = safePosition;
+      volume = safePosition / 4;
+      volume = volume / 2;
+      noInterrupts();
+      Serial.print("calc volume: ");
+      Serial.print(volume);
+      Serial.print(" act volume: ");
+      Serial.print(actualVolume);
+      Serial.println("");
+      changeVolume();
+      interrupts();
     }
-    if(newPosition > 400){
-      safePosition = 400;
-      myEnc.write(safePosition);
+
+    if (!digitalRead(pinButton)) {
+
+      Consumer.write(MEDIA_VOLUME_MUTE);
+      muted = !muted;
+      digitalWrite(L1, HIGH);
+      keyrelease();
     }
-    oldPosition = safePosition;
-    volume = safePosition / 4;
-    volume = volume / 2;
-    noInterrupts();  
-    Serial.print("calc volume: ");
-    Serial.print(volume);
-    Serial.print(" act volume: ");
-    Serial.print(actualVolume);
-    Serial.println("");
-    changeVolume();
-    interrupts();
-  }
-  
-  if (!digitalRead(pinButton)) {
-   
-    Consumer.write(MEDIA_VOLUME_MUTE);
-    muted = !muted;
-    digitalWrite(L1, HIGH);
-    keyrelease();
-  }
-  if (digitalRead(B1) == LOW)
-  {
-    Consumer.write(MEDIA_PREVIOUS);
-    keyrelease();
+    //Previous media
+    if (digitalRead(B1) == LOW)
+    {
+      Consumer.write(MEDIA_PREVIOUS);
+      keyrelease();
 
-  }
-  if (digitalRead(B2) == LOW)
-  {
-    Consumer.write(MEDIA_PLAY_PAUSE);
-    keyrelease();
+    }
+    // play and pause media
+    if (digitalRead(B2) == LOW)
+    {
+      Consumer.write(MEDIA_PLAY_PAUSE);
+      keyrelease();
 
-  }
-  if (digitalRead(B3) == LOW)
-  {
-    Consumer.write(MEDIA_NEXT);
-    keyrelease();
+    }
+    // Next media
+    if (digitalRead(B3) == LOW)
+    {
+      Consumer.write(MEDIA_NEXT);
+      keyrelease();
 
-  }
-  if (digitalRead(B4) == LOW)
-  {
-    app = "Terminal";
-    spotlight(app);
-    keyrelease();
+    }
+    //Open terminal
+    if (digitalRead(B4) == LOW)
+    {
+      app = "Terminal";
+      spotlight_mac(app);
+      keyrelease();
 
+    }
+    //Opening Unomaha Canvas page
+      if (digitalRead(B5) == LOW)
+    {
+      url = "https://unomaha.instructure.com/";
+      chrome_mac(url);
+      keyrelease();
+    }
+    //Taking screenshot on a MAC
+      if (digitalRead(B6) == LOW)
+    {
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press('4');
+      keyrelease();
+    }
   }
+  }
+  else{
+    int newPosition = myEnc.read();
+    if (newPosition != oldPosition)
+    {
+      Serial.println("new");
+      safePosition = newPosition;
+      if(newPosition < 0){
+        safePosition = 0;
+        myEnc.write(safePosition);
+      }
+      if(newPosition > 400){
+        safePosition = 400;
+        myEnc.write(safePosition);
+      }
+      oldPosition = safePosition;
+      volume = safePosition / 4;
+      volume = volume / 2;
+      noInterrupts();
+      Serial.print("calc volume: ");
+      Serial.print(volume);
+      Serial.print(" act volume: ");
+      Serial.print(actualVolume);
+      Serial.println("");
+      changeVolume();
+      interrupts();
+    }
+    if (!digitalRead(pinButton))
+    {
+
+      Consumer.write(MEDIA_VOLUME_MUTE);
+      muted = !muted;
+      digitalWrite(L1, HIGH);
+      keyrelease();
+    }
+    //Previous media
+    if (digitalRead(B1) == LOW)
+    {
+      Consumer.write(MEDIA_PREVIOUS);
+      keyrelease();
+
+    }
+    //Play and stop media
+    if (digitalRead(B2) == LOW)
+    {
+      Consumer.write(MEDIA_PLAY_PAUSE);
+      keyrelease();
+
+    }
+    //Next media
+    if (digitalRead(B3) == LOW)
+    {
+      Consumer.write(MEDIA_NEXT);
+      keyrelease();
+
+    }
+    //Open specific app
+    if (digitalRead(B4) == LOW)
+    {
+      app = "xxxxxxx";
+      start_win(app);
+      keyrelease();
+    }
+    //Open specific website
     if (digitalRead(B5) == LOW)
-  {
-    url = "https://unomaha.instructure.com/";
-    chrome(url);
-    keyrelease();
-
-  }
+    {
+      url = "google.com";
+      chrome_win();
+      keyrelease();
+    }
+    //Screenshot on win10
     if (digitalRead(B6) == LOW)
-  {
-    Keyboard.press(KEY_LEFT_GUI);
-    Keyboard.press(KEY_LEFT_SHIFT);
-    Keyboard.press('4');
-    keyrelease();
+    {
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press('s');
+      keyrelease();
+    }
   }
-}
+
 //////////////////////////////////////////////////////
-//Functions
+//Tools
 //////////////////////////////////////////////////////
 
 void changeVolume(){
 if (volume != oldVolume) {
     if(volume > oldVolume){
       //delay(100);
-      Consumer.write(MEDIA_VOLUME_UP);
+      //Consumer.write(MEDIA_VOLUME_UP);
+
+      String app = "spotify";
+      spotlight_mac(app);
+      delay(300);
+
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press(KEY_UP_ARROW);
+      digitalWrite(L1, HIGH);
+      delay(100);
+      Keyboard.releaseAll();
+      digitalWrite(L1, LOW);
       actualVolume = actualVolume + 2;
-      oldVolume = volume; 
+      oldVolume = volume;
+      delay(500);
     }
     else{
       //delay(100);
-      Consumer.write(MEDIA_VOLUME_DOWN);
+      //Consumer.write(MEDIA_VOLUME_DOWN);
+      String app = "spotify";
+      spotlight_mac(app);
+      delay(300);
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press(KEY_DOWN_ARROW);
+      delay(300);
+      digitalWrite(L1, HIGH);
+      delay(100);
+      Keyboard.releaseAll();
+      digitalWrite(L1, LOW);
       actualVolume = actualVolume - 2;
-      oldVolume = volume; 
+      oldVolume = volume;
+      delay(300);
     }
   }
 }
-void spotlight(String app)
+void spotlight_mac(String app)
 {
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.press(KEY_SPACE);
@@ -174,14 +286,27 @@ void spotlight(String app)
     delay(200);
     Keyboard.releaseAll();
 }
-void chrome(String url){
+
+void start_win(String app)
+{
+    Keyboard.press(KEY_LEFT_GUI);
+    delay(500);
+    Keyboard.releaseAll();
+    delay(200);
+    Keyboard.println(app);
+    delay(1000);
+    Keyboard.press(KEY_ENTER);
+    delay(200);
+}
+
+void chrome_mac(String url){
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.press(KEY_SPACE);
     delay(200);
     Keyboard.releaseAll();
-    Keyboard.println("chrome");
+    Keyboard.println("chrome_mac");
     delay(300);
-    
+
     Keyboard.press(KEY_RETURN);
     delay(300);
     Keyboard.releaseAll();
@@ -199,6 +324,22 @@ void chrome(String url){
     delay(300);
     Keyboard.releaseAll();
 }
+void chrome_win(String url){
+    Keyboard.press(KEY_LEFT_GUI);
+    delay(500);
+    Keyboard.releaseAll();
+    delay(200);
+    Keyboard.println("Google Chrome");                     //HERE IS WHERE YOU WILL CHANGE THE CHARACTERS INSIDE THE "____" TO BE THE WEB BROWSER YOU USE.
+    delay(1000);                                          //YOU MIGHT HAVE TO MESS AROUND WITH THE DELAY AMOUNTS FOR YOUR SPECIFIC COMPUTER.
+    Keyboard.press(KEY_ENTER);
+    Keyboard.releaseAll();
+    delay(1500);
+    Keyboard.println(url);
+    delay(1000);
+    Keyboard.press(KEY_ENTER);
+    delay(200);
+}
+
 void keyrelease(){
     digitalWrite(L1, HIGH);
     delay(100);
